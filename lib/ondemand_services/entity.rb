0fad6c7f21b27c426e33051b81eecb749656344a
Rigel St. Pierre <rigel@rigel.co>
2016-03-14 17:38:51 -0600

    rough entity enpoints, tests still broken

--- lib/ondemand_services/entity.rb
+++ lib/ondemand_services/entity.rb
@@ -1,15 +1,41 @@
+require 'json'
 require 'httparty'
+require 'ostruct'
+require 'securerandom'
+
 
 module OndemandServices
   class Client
     module Entity
 
       def all(publication_id)
+        base_url = OndemandServices::Default::PRODUCER_SERVICE_ENDPOINT + '/publication/' + publication_id
+        headers = {
+          'x-dps-api-key' => client_id,
+          'x-dps-client-version' => OndemandServices::VERSION,
+          'x-dps-client-request-id' => SecureRandom.uuid,
+          'x-dps-client-session-id' => session_id,
+          'c-dps-client-id' => client_id,
+          'authorization' => access_token
+        }
 
+        response = HTTParty.get(base_url, headers: headers)
+        JSON.parse(response.to_json, object_class: OpenStruct)
       end
 
-      def find(publication_id, entity_id)
+      def find(publication_id, entity_type)
+        base_url = OndemandServices::Default::PRODUCER_SERVICE_ENDPOINT + '/publication/' + publication_id
+        headers = {
+          'x-dps-api-key' => client_id,
+          'x-dps-client-version' => OndemandServices::VERSION,
+          'x-dps-client-request-id' => SecureRandom.uuid,
+          'x-dps-client-session-id' => session_id,
+          'c-dps-client-id' => client_id,
+          'authorization' => access_token
+        }
 
+        response = HTTParty.get(base_url, headers: headers)
+        JSON.parse(response.to_json, object_class: OpenStruct)
       end
 
     end
