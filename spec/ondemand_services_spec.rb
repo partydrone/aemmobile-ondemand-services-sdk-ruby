0f09f3f1659ec55077222caa87c03f8a6c64cef4
Andrew Porter <andrew.porter@wavetronix.com>
2016-03-15 21:21:53 -0600

    Add some tests to the root module
    Include some client tests based on `octokit.rb` gem.

--- spec/ondemand_services_spec.rb
+++ spec/ondemand_services_spec.rb
@@ -15,6 +15,26 @@ describe OndemandServices do
     end
   end
 
+  describe ".client" do
+    it "creates an OndemandServices::Client" do
+      OndemandServices.client.must_be_kind_of OndemandServices::Client
+    end
+
+    it "caches the client when the same options are passed" do
+      OndemandServices.client.must_equal OndemandServices.client
+    end
+
+    it "returns a fresh client when options are not the same" do
+      client = OndemandServices.client
+      OndemandServices.client_secret = '003aaa97-d965-4728-8992-bbd2ae7c7a4a'
+      client_two = OndemandServices.client
+      client_three = OndemandServices.client
+
+      client.wont_equal client_two
+      client_three.must_equal client_two
+    end
+  end
+
   describe ".configure" do
     OndemandServices::Configurable.keys.each do |key|
       it "sets the #{key.to_s.gsub('_', ' ')}" do
